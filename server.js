f1de8407759cf41f7ecc6bdbb62bbf67d6dae181
Paul Varner <fuzzyray@gmail.com>
2020-08-17 18:11:16 -0500

    Set up a Template Engine

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.set('view engine', 'pug');
 
 app.route('/').get((req, res) => {
-  //Change the response to render the Pug template
-  res.send(`Pug template is not defined.`);
+  res.render('./pug/index.pug');
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
