cd1d524a3ec165627122fd5430e3a4bffd0f75fe
Paul Varner <fuzzyray@gmail.com>
2020-08-17 21:40:21 -0500

    Registration of New Users

--- server.js
+++ server.js
@@ -78,6 +78,7 @@ client.connect((err) => {
           title: 'Hello',
           message: 'Please login',
           showLogin: true,
+          showRegistration: true,
         };
         res.render(`${process.cwd()}/views/pug/index.pug`, mainVars);
       });
@@ -100,6 +101,32 @@ client.connect((err) => {
         req.logout();
         res.redirect('/');
       });
+
+    app.route('/register')
+      .post((req, res, next) => {
+          db.collection('users')
+            .findOne({username: req.body.username}, (err, user) => {
+              if (err) {
+                next(err);
+              } else if (user) {
+                res.redirect('/');
+              } else {
+                db.collection('users').insertOne({
+                  username: req.body.username,
+                  password: req.body.password,
+                }, (err, doc) => {
+                  if (err) {
+                    res.redirect('/');
+                  } else {
+                    next(null, user);
+                  }
+                });
+              }
+            });
+        }, passport.authenticate('local', authOptions), (req, res, next) => {
+          res.redirect('profile');
+        },
+      );
 
     app.use((req, res, next) => {
       res.status(404)
